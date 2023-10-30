24019347fdeff83958f378a9a37354e8606c1b83
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-10-30 15:41:11 +0100

    Aggiungere i file di progetto.

---
+++ Esercizi C#/Program.cs
@@ -0,0 +1,28 @@
+﻿
+// ** Inizio utilizzo C#** //
+
+using System;
+
+namespace CorsoCsharp;
+
+class Program
+{
+    static void Main(string[]args)
+    {
+        // Variabile 
+        string nome = "Vincenzo";
+
+        string cognome = "Minardi";
+
+        int età = 24
+       
+        // **********
+        Console.WriteLine("Ciao io sono" + nome);
+        Console.WriteLine("Ho" + età + "anni");
+        Console.WriteLine("Questa è la mia prima volta con C#);
+        Console.Read();
+    }
+}
+
+
+
